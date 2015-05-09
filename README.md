d4d43c98fe19f9ae93f8fa910af0828553753bcb
Marcin Kurczewski <mkurczew@gmail.com>
2015-05-09 18:29:33 +0200

    Added image converter

--- README.md
+++ README.md
@@ -9,6 +9,30 @@ BIN archive packer and unpacker. Does not convert images for performance
 reasons: if you often need to repack, converting *all* images would take too
 much time. Instead, you select one image to convert, and then pack the files.
 
+NOTE: when packing, make sure the folder doesn't contain any files other than
+files unpacked from original archive, otherwise the game will crash. This is a
+limitation of FVP games since they expect a fixed number of files.
+
+### `nvsg_converter`
+
+Image converter. Converts image files to png and vice versa. For mass
+conversion, use shell scripts:
+
+    mkdir converted
+    cd folder
+    for x in *; do
+        ../nvsg_converter --decode "$x" ../converted/"$x.png"
+    done
+
+will convert everything in folder to PNG.
+
+Caveats:
+
+1. NVSG images have no extensions (like pretty much everything else in FVP
+   games).
+2. You must pass `-x` and `-y`, and optionally `--image_count` when encoding
+   images back, or there are good chances your graphic won't be rendered.
+
 ### `hcb_compiler`
 
 Script (de)compiler (original author -
--- open_ext.py
+++ open_ext.py
@@ -25,8 +25,45 @@ class open_ext:
             byte = self.file.read(1)
         return out
 
+    def read_until_end(self):
+        return self.file.read()
+
+    def read_u16_le(self):
+        return struct.unpack('<H', self.file.read(2))[0]
+
     def read_u32_le(self):
         return struct.unpack('<I', self.file.read(4))[0]
 
+    def write_u16_le(self, x):
+        self.file.write(struct.pack('<H', x))
+
     def write_u32_le(self, x):
         self.file.write(struct.pack('<I', x))
+
+    class ArgParser(object):
+        def __init__(self, mode='r', bufsize=None):
+            self._mode = mode
+            self._bufsize = bufsize
+
+        def __call__(self, string):
+            # the special argument "-" means sys.std{in,out}
+            if string == '-':
+                if 'r' in self._mode:
+                    return _sys.stdin
+                elif 'w' in self._mode:
+                    return _sys.stdout
+                else:
+                    msg = _('argument "-" with mode %r' % self._mode)
+                    raise ValueError(msg)
+
+            # all other arguments are used as file names
+            if self._bufsize:
+                return open_ext(string, self._mode, self._bufsize)
+            else:
+                return open_ext(string, self._mode)
+
+        def __repr__(self):
+            args = [self._mode, self._bufsize]
+            args_str = ', '.join([repr(arg) for arg in args if arg is not None])
+            return '%s(%s)' % (type(self).__name__, args_str)
+
